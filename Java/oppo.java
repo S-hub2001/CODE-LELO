2098db038768d768c9aa28f5c731ebd32827b383
S-hub2001 <souvikd2015416@gmail.com>
2021-11-08 19:38:01 +0530

    JAVA N

--- Java/oppo.java
+++ Java/oppo.java
@@ -9,12 +9,23 @@ public class oppo {
         }
 
     }
+public class oppo1 {
+    int nat(int j) {
+        if (j == 1) {
+            return 1;
+        } else {
+            return j + nat(j - 1);
+        }
+
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         oppo st = new oppo();
+        oppo1 st1 = new oppo1();
         System.out.println("Total " + st.nat(n));
+        System.out.println("Total " + st1.nat(n));
 
     }
 
